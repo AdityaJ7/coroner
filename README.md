a788c768645d3fea8f103254efd27cff5327ec1b
conner-calhoun <conner610@gmail.com>
2020-09-12 19:00:05 +0000

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Coroner
+
+A simple tool used to determine when your favorite twitch streamers go live.
+
+
+#### VENV
+
+Set up the virtual environment like so:
+
+1. `python3 -m venv ./venv`
+2. `source venv/bin/activate`
---
+++ coroner.py
@@ -0,0 +1,15 @@
+import argparse
+import logging
+
+
+class Coroner:
+    ''' Main Coroner Class'''
+
+    def __init__(self):
+        logging.debug("Coroner Constructed")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='[%(levelname)s] %(filename)s:%(lineno)s: %(message)s', level=logging.DEBUG)
+
+    coroner = Coroner()
