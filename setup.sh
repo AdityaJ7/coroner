e1523177d0d3794d0d174cbc92f869bb7f206f66
conner-calhoun <conner610@gmail.com>
2020-09-15 03:21:02 +0000

    remove underscore from pythonpath in setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 THIS_DIR=$(dirname $(readlink -f $BASH_SOURCE))
 pushd $THIS_DIR
 
-export PYTHONPATH=$THIS_DIR/src:$PYTHON_PATH
+export PYTHONPATH=$THIS_DIR/src:$PYTHONPATH
 source venv/bin/activate
 
 echo -e "\033[32mSetup Complete\033[0m"
